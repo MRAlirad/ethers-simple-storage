5a0c8286acf3a68b77949f943b56adcad69c2eaf
MRAlirad <m.r.alirad75@gmail.com>
2024-02-02 15:13:24 +0330

    transaction receipts

--- Deploy.js
+++ Deploy.js
@@ -2,7 +2,6 @@ const ethers = require('ethers');
 const fs = require('fs');
 
 async function main() {
-	console.log(ethers.ethers);
 	const provider = new ethers.JsonRpcProvider('http://127.0.0.1:7545');
 	const wallet = new ethers.Wallet(
 		'0xee8c0e71136cde4ddf3e0ff7d1bb62a90f33282845bd610472ac14722c9d64cc',
@@ -12,8 +11,16 @@ async function main() {
 	const binary = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.bin', 'utf-8');
 	const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
 	console.log('Deploying, please wait...');
+
+	// deploy the contract
 	const contract = await contractFactory.deploy();
-	console.log(contract);
+
+	// deploy transaction will be waiting to be mined (but not yet included in a block)
+	const transactionReceipt = await contract.deploymentTransaction().wait(1);
+	console.log('here is the dployment transaction (transaciton response): ');
+	console.log(contract.deploymentTransaction);
+	console.log('here is the transaction receipt');
+	console.log(transactionReceipt); // the receipt of transaction
 }
 
 main()
