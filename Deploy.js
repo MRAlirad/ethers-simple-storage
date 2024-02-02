29df6b8f08f0f6d4e19c0914e5eb676d2bab8ab5
MRAlirad <m.r.alirad75@gmail.com>
2024-02-02 13:57:57 +0330

    deploy contract to local blockchain with ethers.js

--- Deploy.js
+++ Deploy.js
@@ -1,1 +1,26 @@
-console.log('sf')
+const ethers = require('ethers');
+const fs = require('fs');
+
+async function main() {
+	console.log(ethers.ethers);
+	const provider = new ethers.JsonRpcProvider('http://127.0.0.1:7545');
+	const wallet = new ethers.Wallet(
+		'0xee8c0e71136cde4ddf3e0ff7d1bb62a90f33282845bd610472ac14722c9d64cc',
+		provider
+	);
+	const abi = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.abi', 'utf-8');
+	const binary = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.bin', 'utf-8');
+	const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+	console.log('Deploying, please wait...');
+	const contract = await contractFactory.deploy();
+	console.log(contract);
+}
+
+main()
+	.then(() => {
+		process.exit(0);
+	})
+	.catch(error => {
+		console.log(error);
+		process.exit(1);
+	});
